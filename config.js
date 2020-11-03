c44bfc8ccdd0a22b0a3b39eca54ac20a9d1816ba
sepicharom <72716710+sepicharom@users.noreply.github.com>
2020-11-03 20:12:47 +0330

    Update and rename example.config.js to config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     botToken:'1477159037:AAETQft8eTHcyR5n8rXx-qDunfs0lbVAsuM',
-    bitlyToken:'<bit.ly Token Here>'
+    bitlyToken:'df9a0117c358ecadfb8d2a1af747933522524f30'
 };
